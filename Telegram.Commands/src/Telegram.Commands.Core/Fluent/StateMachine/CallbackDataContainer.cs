f0325b1c2e999790ed33b5a118e8ab880a88136c
Eugene <em.milovidov@gmail.com>
2023-01-28 21:15:01 +0300

    callback with builders

---
+++ Telegram.Commands/src/Telegram.Commands.Core/Fluent/StateMachine/CallbackDataContainer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Telegram.Commands.Abstract.Interfaces;
+using Telegram.Commands.Core.Services;
+
+namespace Telegram.Commands.Core.Fluent.StateMachine;
+
+internal class CallbackDataContainer
+{
+    private readonly Func<IEnumerable<IEnumerable<CallbackDataWithCommand>>> _builder;
+    private readonly CallbackDataWithCommand[] _callbackData;
+
+    public CallbackDataContainer(CallbackDataWithCommand callbackData)
+    {
+        _callbackData = new[] { callbackData };
+    }
+
+    public CallbackDataContainer(CallbackDataWithCommand[] callbackData)
+    {
+        _callbackData = callbackData;
+    }
+
+    public CallbackDataContainer(Func<IEnumerable<IEnumerable<CallbackDataWithCommand>>> builder)
+    {
+        _builder = builder;
+    }
+
+    public IEnumerable<IEnumerable<CallbackDataWithCommand>> Build()
+    {
+        if (_callbackData != null && _callbackData.Any())
+            return new[] { _callbackData };
+
+        if (_builder != null)
+            return _builder();
+
+        throw new InvalidOperationException();
+    }
+
+    public bool HasCommand(ITelegramCommandDescriptor descriptor)
+    {
+        if (_callbackData.Any(x => x.CommandDescriptor != null && x.CommandDescriptor.MatchCommand(descriptor)))
+            return true;
+
+        if (_builder != null)
+        {
+            var callbacks = _builder();
+            return callbacks.Select(callback => callback.Any(x => x.CommandDescriptor != null && x.CommandDescriptor.MatchCommand(descriptor))).FirstOrDefault();
+        }
+
+        return false;
+    }
+}
--- Telegram.Commands/src/Telegram.Commands.Core/Fluent/StateMachine/State.cs
+++ Telegram.Commands/src/Telegram.Commands.Core/Fluent/StateMachine/State.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
-using Telegram.Commands.Abstract.Interfaces;
 using Telegram.Commands.Abstract.Interfaces.Commands;
 using Telegram.Commands.Core.Services;
 
@@ -106,51 +105,5 @@ internal class State<TObj> : IState<TObj>
     public void AddCallback(Func<IEnumerable<IEnumerable<CallbackDataWithCommand>>> builder)
     {
         _callbacksContainers.Add(new CallbackDataContainer(builder));
-    }
-}
-
-internal class CallbackDataContainer
-{
-    private readonly Func<IEnumerable<IEnumerable<CallbackDataWithCommand>>> _builder;
-    private readonly CallbackDataWithCommand[] _callbackData;
-
-    public CallbackDataContainer(CallbackDataWithCommand callbackData)
-    {
-        _callbackData = new[] { callbackData };
-    }
-
-    public CallbackDataContainer(CallbackDataWithCommand[] callbackData)
-    {
-        _callbackData = callbackData;
-    }
-
-    public CallbackDataContainer(Func<IEnumerable<IEnumerable<CallbackDataWithCommand>>> builder)
-    {
-        _builder = builder;
-    }
-
-    public IEnumerable<IEnumerable<CallbackDataWithCommand>> Build()
-    {
-        if (_callbackData != null && _callbackData.Any())
-            return new[] { _callbackData };
-
-        if (_builder != null)
-            return _builder();
-
-        throw new InvalidOperationException();
-    }
-
-    public bool HasCommand(ITelegramCommandDescriptor descriptor)
-    {
-        if (_callbackData.Any(x => x.CommandDescriptor != null && x.CommandDescriptor.MatchCommand(descriptor)))
-            return true;
-
-        if (_builder != null)
-        {
-            var callbacks = _builder();
-            return callbacks.Select(callback => callback.Any(x => x.CommandDescriptor != null && x.CommandDescriptor.MatchCommand(descriptor))).FirstOrDefault();
-        }
-
-        return false;
     }
 }
