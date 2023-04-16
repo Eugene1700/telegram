4939919f7f2590adf9a46f3b8f6c5d08d333834f
Eugene <em.milovidov@gmail.com>
2023-04-16 10:08:59 +0300

    desc ISessionManager.cs

--- Telegram.Commands/src/Telegram.Commands.Abstract/Interfaces/ISessionManager.cs
+++ Telegram.Commands/src/Telegram.Commands.Abstract/Interfaces/ISessionManager.cs
@@ -4,15 +4,31 @@ using Telegram.Commands.Abstract.Interfaces.Commands;
 
 namespace Telegram.Commands.Abstract.Interfaces
 {
-    //todo
     /// <summary>
-    /// Services 
+    /// Interface to access to session manager from user's code
     /// </summary>
     public interface ISessionManager
     {
+        /// <summary>
+        /// Get current session for <see cref="ISessionTelegramCommand{TQuery,TSessionObject}"/>
+        /// </summary>
+        /// <param name="chatId">chat, where are doing conversation</param>
+        /// <param name="telegramUserId">telegram user id, which initiate conversation</param>
+        /// <typeparam name="TCommand">Type of session command</typeparam>
+        /// <typeparam name="TQuery">Type of user's request</typeparam>
+        /// <typeparam name="TData">Type of user's data</typeparam>
+        /// <returns>session data <see cref="ISessionInfoWithData"/></returns>
         ISessionInfoWithData<TData> GetSession<TCommand, TQuery, TData>(long chatId, long telegramUserId)
             where TCommand : ISessionTelegramCommand<TQuery, TData>;
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="chatId"></param>
+        /// <param name="telegramUserId"></param>
+        /// <typeparam name="TCommand"></typeparam>
+        /// <typeparam name="TData"></typeparam>
+        /// <returns></returns>
         ISessionInfoWithData<TData> GetSession<TCommand, TData>(long chatId, long telegramUserId)
             where TCommand : IBehaviorTelegramCommand<TData>;
 
